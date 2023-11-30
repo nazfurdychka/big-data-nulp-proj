706e5b069ab841ebeb710fa30d3eb9e61655c46d
Anton_Git <antoshkvir@gmail.com>
2023-11-30 05:04:06 +0200

    Fixed bug when Spark couldn't write data into files. Improved DAG to check Spark work. Other minor fixes and changes.

--- dags/dag_spark_test.py
+++ dags/dag_spark_test.py
@@ -1,10 +1,9 @@
 import os
 
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
-from datetime import datetime, timedelta
+from datetime import datetime
 
 default_args = {
     'owner': 'airflow',
@@ -12,17 +11,21 @@ default_args = {
 }
 
 with DAG(
-    'PYSPARK_TEST2',
+    'PySparkTest',
     default_args=default_args,
     start_date=datetime(2023, 1, 1),
     description='DAG to check the connection to Spark cluster',
     schedule_interval=None,
 ) as dag:
-    spark_apps = os.path.join('/opt', 'spark_apps')
+    start_operator = DummyOperator(task_id='start')
 
+    spark_apps = os.path.join('/opt', 'spark_apps')
     spark_operator = SparkSubmitOperator(
-        task_id='test_task',
+        task_id='spark_test',
         application=os.path.join(spark_apps, 'spark_app_test.py'),
         conn_id='spark_default',
-        proxy_user='root'
     )
+
+    end_operator = DummyOperator(task_id='end')
+
+    start_operator >> spark_operator >> end_operator
