6554cc0b28dddb4cfb136cdea84f56f8de54249e
nazarmac <nazar.furdychka50@gmail.com>
2023-12-01 13:52:11 +0200

    changed dag

--- dags/dags.py
+++ dags/dags.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 DEFAULT_CONNECTION_ID = 'spark_default'
 SPARK_JOBS_PARENT_DIR_PATH = os.path.join('/opt', 'spark_apps')
+BUSINESS_QUESTIONS_LINK = 'https://docs.google.com/document/d/1y8T0asC7HBBuPaWgVDPY9YFUX3ETVNiRjbvvQgXN9qM'
 
 default_args = {
     'owner': 'airflow',
@@ -14,15 +15,15 @@ default_args = {
 }
 
 with DAG(
-        'Question_Set_1',
+        'business-questions-1',
         default_args=default_args,
         start_date=datetime(2023, 1, 1),
-        description='DAG to check custom codde',
+        description=f'Owned by Nazar Furdychka, questions can be accessed by : {BUSINESS_QUESTIONS_LINK}',
         schedule_interval=None,
 ) as dag:
     start_operator = DummyOperator(task_id='start')
     spark_operator = SparkSubmitOperator(
-        task_id='spark_test',
+        task_id='spark-template',
         application=os.path.join(SPARK_JOBS_PARENT_DIR_PATH, 'spark_job_template.py'),
         conn_id=DEFAULT_CONNECTION_ID,
     )
@@ -32,15 +33,15 @@ with DAG(
     start_operator >> spark_operator >> end_operator
 
 with DAG(
-        'Question_Set_2',
+        'business-questions-2',
         default_args=default_args,
         start_date=datetime(2023, 1, 1),
-        description='DAG to check custom codde',
+        description=f'Owned by ______, questions can be accessed by : {BUSINESS_QUESTIONS_LINK}',
         schedule_interval=None,
 ) as dag:
     start_operator = DummyOperator(task_id='start')
     spark_operator = SparkSubmitOperator(
-        task_id='spark_test',
+        task_id='spark-template',
         application=os.path.join(SPARK_JOBS_PARENT_DIR_PATH, 'spark_job_template.py'),
         conn_id=DEFAULT_CONNECTION_ID,
     )
